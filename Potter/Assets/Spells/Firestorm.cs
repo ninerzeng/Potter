af171f16a3c81d485f4ea099231a0138a9db7a66
Nina <ninazeng@umich.edu>
2015-01-17 17:54:29 -0500

    firestorm cs code

--- Potter/Assets/Spells/Firestorm.cs
+++ Potter/Assets/Spells/Firestorm.cs
@@ -1,14 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Firestorm : SpellBehavior {
 
+public class Firestorm : SpellBehavior {
+	public GameObject nowSpellObj;
+	public float spawnRadius;
+	void Awake()
+	{
+		Application.runInBackground = true;
+	}
 	public override void Attack ()
 	{
-		if(Input.GetKeyDown(KeyCode.F))
-		{
 
+		for(int i = 0; i <20; i++)
+		{
+			Vector3 spawnPosition = new Vector3(Random.insideUnitSphere.x * spawnRadius + this.transform.position.x, 
+			                                    transform.position.y+ 4.098938f, this.transform.position.z + Random.insideUnitSphere.z * spawnRadius);
+
+			Quaternion myrotate = this.transform.rotation;
+			myrotate.eulerAngles = new Vector3(90f, 180f, 0);
+	
+	
+			GameObject fireball = Instantiate (nowSpellObj, spawnPosition, myrotate) as GameObject;
+			
+			//		Instantiate(nowSpellObj);
+			StartCoroutine(TestCoroutine(fireball));
+//				TestCoroutine (fireball);
 		}
+//		Instantiate (nowSpellObj);
+
+//		Destroy(myparticle);
+	}
+	IEnumerator TestCoroutine(GameObject ToBeDestroyed)
+	{
+		yield return new WaitForSeconds(3.0f);
+		Destroy (ToBeDestroyed);
+
 	}
 	// Use this for initialization
 	void Start () {
@@ -17,6 +44,10 @@ public class Firestorm : SpellBehavior {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			print ("Fire!");
+			Attack();
+		}
 	}
 }
