d220d1d520ee67e886815d0818c741bc4d2b3177
Mike Vianueva <vianuevm@umich.edu>
2015-01-18 11:09:50 -0500

    flight and whales

---
+++ Potter/Assets/Humpback whale/Whale.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BoxCollider c = transform.GetComponent<BoxCollider>();
+		c.rigidbody.velocity = Vector3.back * 10;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	
+	}
+}
