444db1c390d3b2cf83a30bb473404920d20fa0e9
Mike Vianueva <vianuevm@umich.edu>
2015-02-10 18:58:35 -0500

    stuff

---
+++ Potter/Assets/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	public float health = 100;
+	GUIBarScript GUIBar;
+
+	// Use this for initialization
+	void Start () {
+		GUIBar = GetComponentInChildren<GUIBarScript>();
+		health= 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			GUIBar.Value = health / 100f;
+		if (health <=0)
+			GUIBar.end = true;
+	}
+}
---
+++ Potter/Assets/Spells/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "Player"){
+			Application.LoadLevel("Demo_Scene");
+		}
+	}
+}
